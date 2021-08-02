e133de67787438a20183b68f03390967515f53f9
Steven Geweiler <stevengeweiler@icloud.com>
2021-08-02 11:44:31 +0200

    ...

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ if (os.platform() === 'linux') {
     if (value === 1)
       ControlDate = Date.now();
 
-    if (value === 0 && ControlDate - 3000 < Date.now())
+    if (value === 0 && ControlDate - 3000 > Date.now())
       return console.log('Korrektur wird gesendet')
   });
 
