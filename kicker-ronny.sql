37c61efe365901577a5cb88aedb6e2685a4c8ff7
Steven Geweiler <stevengeweiler@icloud.com>
2021-08-05 15:05:46 +0200

    Added current database

--- kicker-ronny.sql
+++ kicker-ronny.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 27. Jul 2021 um 10:44
--- Server-Version: 10.4.11-MariaDB
--- PHP-Version: 7.4.5
+-- Erstellungszeit: 05. Aug 2021 um 15:05
+-- Server-Version: 10.4.17-MariaDB
+-- PHP-Version: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,11 +29,47 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `gameplan` (
   `id` int(11) NOT NULL,
-  `first_team` int(11) NOT NULL,
-  `second_team` int(11) NOT NULL,
+  `first_team` varchar(255) NOT NULL,
+  `second_team` varchar(255) NOT NULL,
   `first_team_score` int(11) NOT NULL,
   `second_team_score` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `gameplan`
+--
+
+INSERT INTO `gameplan` (`id`, `first_team`, `second_team`, `first_team_score`, `second_team_score`) VALUES
+(1, 'Steven', 'Horst', 0, 0),
+(2, 'Mario', 'Dieter', 0, 0),
+(3, 'Tom', 'Klaus', 0, 0),
+(4, 'Steven', 'Mario', 0, 0),
+(5, 'Tom', 'Horst', 0, 0),
+(6, 'Klaus', 'Dieter', 0, 0),
+(7, 'Steven', 'Tom', 0, 0),
+(8, 'Klaus', 'Mario', 0, 0),
+(9, 'Dieter', 'Horst', 0, 0),
+(10, 'Steven', 'Klaus', 0, 0),
+(11, 'Dieter', 'Tom', 0, 0),
+(12, 'Horst', 'Mario', 0, 0),
+(13, 'Steven', 'Dieter', 0, 0),
+(14, 'Horst', 'Klaus', 0, 0),
+(15, 'Mario', 'Tom', 0, 0),
+(16, 'Steven', 'Dieter', 0, 0),
+(17, 'Horst', 'Klaus', 0, 0),
+(18, 'Mario', 'Tom', 0, 0),
+(19, 'Steven', 'Klaus', 0, 0),
+(20, 'Dieter', 'Tom', 0, 0),
+(21, 'Horst', 'Mario', 0, 0),
+(22, 'Steven', 'Tom', 0, 0),
+(23, 'Klaus', 'Mario', 0, 0),
+(24, 'Dieter', 'Horst', 0, 0),
+(25, 'Steven', 'Mario', 0, 0),
+(26, 'Tom', 'Horst', 0, 0),
+(27, 'Klaus', 'Dieter', 0, 0),
+(28, 'Steven', 'Horst', 0, 0),
+(29, 'Mario', 'Dieter', 0, 0),
+(30, 'Tom', 'Klaus', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -56,6 +92,18 @@ CREATE TABLE `players` (
   `crank` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Daten für Tabelle `players`
+--
+
+INSERT INTO `players` (`id`, `name`, `icon`, `color`, `wins`, `draws`, `defeat`, `goals`, `countergoals`, `score`, `games`, `crank`) VALUES
+(1, 'Steven', 'rocket', '#3472a2', 0, 0, 0, 0, 0, 0, 0, 0),
+(3, 'Mario', 'cat', '#000000', 0, 0, 0, 0, 0, 0, 0, 0),
+(7, 'Tom', 'cat', '#c67b7b', 0, 0, 0, 0, 0, 0, 0, 0),
+(14, 'Klaus', '', '#000000', 0, 0, 0, 0, 0, 0, 0, 0),
+(16, 'Dieter', 'eye', '#db4d4d', 0, 0, 0, 0, 0, 0, 0, 0),
+(17, 'Horst', 'cow', '#991a1a', 0, 0, 0, 0, 0, 0, 0, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -70,6 +118,15 @@ CREATE TABLE `settings` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Daten für Tabelle `settings`
+--
+
+INSERT INTO `settings` (`id`, `mode`, `title`, `playtime`) VALUES
+(1, 1, 'Kicker Ronny', 166),
+(5, 1, 'Weihnachtsfeier 2021', 185),
+(6, 1, 'Weihnachts Ronny 21', 210);
+
+--
 -- Indizes der exportierten Tabellen
 --
 
@@ -77,9 +134,7 @@ CREATE TABLE `settings` (
 -- Indizes für die Tabelle `gameplan`
 --
 ALTER TABLE `gameplan`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `first_team` (`first_team`),
-  ADD KEY `second_team` (`second_team`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indizes für die Tabelle `players`
@@ -101,30 +156,19 @@ ALTER TABLE `settings`
 -- AUTO_INCREMENT für Tabelle `gameplan`
 --
 ALTER TABLE `gameplan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT für Tabelle `players`
 --
 ALTER TABLE `players`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT für Tabelle `settings`
 --
 ALTER TABLE `settings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints der exportierten Tabellen
---
-
---
--- Constraints der Tabelle `gameplan`
---
-ALTER TABLE `gameplan`
-  ADD CONSTRAINT `gameplan_ibfk_1` FOREIGN KEY (`first_team`) REFERENCES `players` (`id`),
-  ADD CONSTRAINT `gameplan_ibfk_2` FOREIGN KEY (`second_team`) REFERENCES `players` (`id`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
